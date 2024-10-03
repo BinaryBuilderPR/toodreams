2c24bbc758ff71aac8e69e28bf52a3e60defd999
Piyush Ranjan <devevloper.piyushranjan@gmail.com>
2024-10-03 20:53:16 +0530

    update header

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import classes from './Header.module.css'; // Import the CSS module
-import "../../Global.css"
+import { Class, Search } from '@mui/icons-material';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+// import "../../Global.css"
 const Header = () => {
   return (
     <header className={classes.header}>
@@ -11,16 +14,19 @@ const Header = () => {
       </div>
 
         <div className={classes.input_div}>
+          <Search fontSize="large" className={classes.input_logo}/>
         <input  type="text" placeholder="Search for products..." className={classes.input}></input>
 
         </div>
       <nav className={classes.navbar}>
-        <ul>
+        <ul >
           <li>Become Seller</li>
           <li>Row Materials</li>
           <li>Specialist & Works </li>
           <li>Plumbing & Electrical </li>
           <li>Contact</li>
+         <li><ShoppingCartOutlinedIcon fontSize='large' /></li>
+         <li><AccountCircleOutlinedIcon fontSize='large'/></li>
         </ul>
       </nav>
     </header>
