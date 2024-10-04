376c9ba3e05cbb0e841d9ab8eba8e375cda1789c
Piyush Ranjan <devevloper.piyushranjan@gmail.com>
2024-10-04 21:45:46 +0530

    add hero images

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,12 +3,15 @@ import classes from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
+    <div className={classes.Hero_main}>
     <div className={classes.Hero_section}>
-        <h1 className={classes.Hero_text}>On-Time, Every Time – Raw Materials and Construction Items at Your Doorsteps</h1>
-        <div>
+        <h1 className={classes.Hero_text}>On-Time, Every Time – Raw Materials & Construction Items at Your Doorsteps</h1>
+    </div>
 
-            <img  className={classes.first_image} src="images/pexels-tima-miroshnichenko-6473974.jpg"></img>
+    <div className={classes.image}>
+          <img src="images/hero images.jpg"></img>
         </div>
+
     </div>
   )
 };
