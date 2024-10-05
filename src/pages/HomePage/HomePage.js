3f8ebab54aba2e3fc8405abc86fd0768b8ef8937
Piyush Ranjan <devevloper.piyushranjan@gmail.com>
2024-10-05 12:16:07 +0530

    fix warnings

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,12 +4,12 @@ import classes from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div className={classes.Hero_main}>
-    <div className={classes.Hero_section}>
-        <h1 className={classes.Hero_text}>On-Time, Every Time – Raw Materials & Construction Items at Your Doorsteps</h1>
-    </div>
+      <div className={classes.Hero_section}>
+          <h1 className={classes.Hero_text}>On-Time, Every Time – Raw Materials & Construction Items at Your Doorsteps</h1>
+      </div>
 
-    <div className={classes.image}>
-          <img src="images/hero images.jpg"></img>
+      <div className={classes.image}>
+            <img src="images/hero images.jpg" alt="hero image"></img>
       </div>
 
     </div>
