4a0a869bcc47a6bd57ceec9368a4ef41213f7bc0
Piyush Ranjan <developer.piyushranjan@gmail.com>
2024-10-06 09:47:16 +0530

    add grid image in

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -8,8 +8,17 @@ const HomePage = () => {
           <h1 className={classes.Hero_text}>On-Time, Every Time – Raw Materials & Construction Items at Your Doorsteps</h1>
       </div>
 
+      <div className={classes.first_image}>
+            <img src="images/electrical-cable-energy-technology-equipment-isolated-white.jpg" alt="hero"></img>
+      </div>
       <div className={classes.image}>
-            <img src="images/hero images.jpg" alt="hero"></img>
+            <img src="images/Gemini_Generated_Image_dv693adv693adv69.jpeg" alt="hero"></img>
+      </div>
+      <div className={classes.image}>
+            <img src="images/Gemini_Generated_Image_fgyaiffgyaiffgya.jpeg" alt="hero"></img>
+      </div>
+      <div className={classes.image}>
+            <img src="images/mix_img.png" alt="hero"></img>
       </div>
 
     </div>
